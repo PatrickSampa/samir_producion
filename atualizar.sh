8d05f0aa1c31cfe915ac89ed4f957c29c1f917e9
Moises Pompilio <93723302+moisesPompilio@users.noreply.github.com>
2023-05-03 16:02:25 -0300

    Update atualizar.sh

--- atualizar.sh
+++ atualizar.sh
@@ -7,16 +7,16 @@ docker-compose up --build -d
 cd ../../
 
 # Executa o cluster no diretório cluster/calculadora_samir/
-docker-compose up --build -d
 cd cluster/calculadora_samir/
+docker-compose up --build -d
 cd ../../
 
 # Executa o cluster no diretório cluster/controle_usuario_samir/
-docker-compose up --build -d
 cd cluster/controle_usuario_samir/
+docker-compose up --build -d
 cd ../../
 
 # Executa o cluster no diretório test_cluster/front
-docker-compose up --build -d
 cd test_cluster/front
+docker-compose up --build -d
 cd ../../
