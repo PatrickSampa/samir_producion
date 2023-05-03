24ab5341c0b48113e928809d00dc2554fc29e0f2
Moises Pompilio <93723302+moisesPompilio@users.noreply.github.com>
2023-05-03 15:44:18 -0300

    feat: create atualizar.sh

---
+++ atualizar.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Atualiza o repositório
+git pull origin main
+
+# Executa o cluster no diretório cluster/front_samir/
+docker-compose -f ./cluster/front_samir/docker-compose.yml up --build -d
+
+# Executa o cluster no diretório cluster/calculadora_samir/
+docker-compose -f ./cluster/calculadora_samir/docker-compose.yml up --build -d
+
+# Executa o cluster no diretório cluster/controle_usuario_samir/
+docker-compose -f ./cluster/controle_usuario_samir/docker-compose.yml up --build -d
+
+# Executa o cluster no diretório test_cluster/front
+docker-compose -f ./test_cluster/front/docker-compose.yml up --build -d
